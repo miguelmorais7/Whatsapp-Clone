be55d4ad59c4aed25b4027beca92e8c5ad924e38
miguelmorais7 <miguelmorais.mcm@gmail.com>
2020-10-29 19:28:38 -0300

    :package: Sidebar e Contentarea em desenvolvimento

--- whatsappclone/src/App.js
+++ whatsappclone/src/App.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import './App.css';
+
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 export default() =>{
   return(
-    <div>Hello World!</div>
+    <div className="app-window">
+      <div className="sidebar">
+        <header>
+          <img className="header--avatar" src="https://i.pinimg.com/originals/51/f6/fb/51f6fb256629fc755b8870c801092942.png"/>
+          <div className="header--buttons">
+            <div className="header--btn">
+              <DonutLargeIcon style={{color: '#919191'}} />
+            </div>
+            <div className="header--btn">
+              <ChatIcon style={{color: '#919191'}} />
+            </div>
+            <div className="header--btn">
+              <MoreVertIcon style={{color: '#919191'}} />
+            </div>
+
+          </div>
+        </header>
+
+        <div className="search">
+          
+        </div>
+
+        <div className="chatlist">
+
+        </div>
+
+
+      </div>
+
+      <div className="contentarea">
+
+      </div>
+    </div>
   );
 }
