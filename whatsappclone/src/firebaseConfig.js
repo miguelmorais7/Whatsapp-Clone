9225699b3ad1445076de5e9d0a331650117efb09
miguelmorais7 <miguelmorais.mcm@gmail.com>
2020-10-30 12:59:35 -0300

    Importação do Firebase

---
+++ whatsappclone/src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default{
+    apiKey: "AIzaSyCbY3H1cNDNjVAlfSixBzs7MDxAngIzbdg",
+    authDomain: "whatsapp-clone-399c7.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-399c7.firebaseio.com",
+    projectId: "whatsapp-clone-399c7",
+    storageBucket: "whatsapp-clone-399c7.appspot.com",
+    messagingSenderId: "554598521404",
+    appId: "1:554598521404:web:54eb804225f01d178e8139"
+  };
